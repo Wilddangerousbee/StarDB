361e44b41e520a31481cc34f248eca5c64905e9a
AKOZACHENKO <wilddangerousbee@gmail.com>
2022-04-09 22:11:21 +0300

    delite console log

--- star-db/src/hoc-helpers/WrapperConsumer.js
+++ star-db/src/hoc-helpers/WrapperConsumer.js
@@ -6,9 +6,6 @@ const WrapperConsumet = (View) => {
     return (props) => (
         <SwapiServiceConsumer> 
         { ({selectionMethod, selectionMethodUncle}) => {
-                console.log(selectionMethod);
-                console.log(selectionMethodUncle);
-                console.log(View);
                 return (<View {...props} selectionMethod = {selectionMethod} 
                         selectionMethodUncle = {selectionMethodUncle}/>
                 )
